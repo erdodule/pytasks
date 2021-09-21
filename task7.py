987ea2f9241638681792e3ca4374551454fa70ff
erdodule <erdo_dule@hotmail.com>
2021-09-21 10:40:25 +0200

    task7

--- task7.py
+++ task7.py
@@ -1,2 +1,18 @@
+from ipaddress import IPv4Address
+import sys
+
+
+try:
+    addr = IPv4Address(input("Please input a vali Ip address:"))
+    if addr < IPv4Address("127.0.0.1"):
+        print("not the correct IP range")
+    elif addr > IPv4Address("255.255.255.255"):
+        print("not the correct IP range")
+    else:
+        print(addr)
+except ValueError:
+    print("Ip address is not valid")
+
+
 
 
