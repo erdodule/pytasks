fa53fbce58c3b3e5d6c0251fda7f812f6c25fe1c
erdodule <erdo_dule@hotmail.com>
2021-09-21 10:46:04 +0200

    task11

--- task11.py
+++ task11.py
@@ -1,10 +1,10 @@
-#import sys
-#import os
-#if __name__=="__main__":
-# if len(sys.argv) > 1:
-#   p = sys.argv[1]
-# else:
-#  p = os.path.abspath("https://github.com/erdodule/pytasks.git")
-#
-# print (p)
+import sys
+import os
+if __name__=="__main__":
+    if len(sys.argv) > 1:
+        p = sys.argv[1]
+    else:
+        p = os.path.abspath("https://github.com/erdodule/pytasks.git")
+
+print(p)
 
